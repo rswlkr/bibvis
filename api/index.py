3850f619d588dadcc378dad7ae348567e95b8245
Ross Walker <ross.walker@costain.com>
2021-11-13 23:18:25 +0000

    test

--- api/index.py
+++ api/index.py
@@ -10,6 +10,22 @@ import pandas as pd
 print(config['Authentication']['APIKey'])
 from pybliometrics.scopus import AbstractRetrieval, ScopusSearch, CitationOverview
 
+def bibvis(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    request_json = request.get_json()
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return f'Hello World!'
 
 #  pub_year = pub['bib']['pub_year']
 #     pub_citations = pub['num_citations']
